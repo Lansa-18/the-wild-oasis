87d424938a69f6f7ac353b0a44769c10971adba3
Mapelujo <olamideiyanda18@gmail.com>
2025-02-14 20:32:10 +0100

    fetching cabin data

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
 
 const TableRow = styled.div`
   display: grid;
@@ -39,14 +40,19 @@ const Discount = styled.div`
   color: var(--color-green-700);
 `;
 
-function CabinRow({cabin}) {
-  const {name, maxCapacity, regularPrice, discount, image} = cabin;
+function CabinRow({ cabin }) {
+  const { name, maxCapacity, regularPrice, discount, image } = cabin;
 
   return (
     <TableRow role="row">
       <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests.</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button>delete</button>
     </TableRow>
-  )
+  );
 }
 
-export default CabinRow
+export default CabinRow;
