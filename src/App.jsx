8e6a22ca345c392664926b9627622b3404cf81c3
Mapelujo <olamideiyanda18@gmail.com>
2025-02-16 09:02:35 +0100

    displaying error and success messsages using toast.

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 // const Dashboard = lazy(() => import("./pages/Dashboard"));
 // const Bookings = lazy(() => import("./pages/Bookings"));
@@ -34,26 +35,46 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false}>
-      </ReactQueryDevtools>
-        <GlobalStyles />
-        <BrowserRouter>
-          <Routes>
-            {/* This is called a layout route because it doesn't have the path prop in it. */}
-            <Route element={<AppLayout />}>
-              <Route index element={<Navigate replace to="dashboard" />} />
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="bookings" element={<Bookings />} />
-              <Route path="cabins" element={<Cabins />} />
-              <Route path="users" element={<Users />} />
-              <Route path="setting" element={<Settings />} />
-              <Route path="account" element={<Account />} />
-            </Route>
+      <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          {/* This is called a layout route because it doesn't have the path prop in it. */}
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="setting" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
 
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
