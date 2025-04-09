999d43ed0cf39119126e60d04a6dfcfbae5f9dfb
Mapelujo <olamideiyanda18@gmail.com>
2025-04-09 04:53:40 +0100

    Client-Side Sorting: Sorting Cabins

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -49,6 +49,7 @@ function Filter({ filterField, options }) {
       {options.map((option) => (
         <FilterButton
           active={option.value === currentFilter}
+          disabled={option.value === currentFilter}
           onClick={() => handleClick(option.value)}
           key={option.value}
         >
